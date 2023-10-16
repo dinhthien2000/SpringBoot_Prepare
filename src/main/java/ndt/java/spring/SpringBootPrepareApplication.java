27cf8d2f780350a676dca0272bf598266fc27cdd
Your Name <you@example.com>
2023-10-16 16:45:10 +0700

    First commit - is root code

---
+++ src/main/java/ndt/java/spring/SpringBootPrepareApplication.java
@@ -0,0 +1,13 @@
+package ndt.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrepareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrepareApplication.class, args);
+	}
+
+}
