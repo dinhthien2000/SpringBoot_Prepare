eab99d2cdcef41b4cb90ff4100ec5301c04b26e5
Alan <thiencamau1804@gmail.com>
2023-10-22 03:31:56 +0700

    Update entity Product and create Two endpoint API of ProductController, they are function 'create' and 'list'

---
+++ src/main/java/ndt/java/spring/controller/ProductController.java
@@ -0,0 +1,40 @@
+package ndt.java.spring.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import ndt.java.spring.enties.Product;
+import ndt.java.spring.repository.ProductRepository;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@PostMapping
+	// ResponseEntity<?> với <?> is a shorthand for <? extends Object>, viết tắt
+	public ResponseEntity<?> create(@RequestBody @Valid Product product) {
+		repository.save(product);
+		URI productUri = URI.create("/product/" + product.getId());
+		// return ResponseEntity.ok(product);
+		return ResponseEntity.created(productUri).body(product);
+		
+	}
+	
+	@GetMapping
+	public List<Product> list() {
+		return repository.findAll();
+	}
+}
--- src/main/java/ndt/java/spring/enties/Product.java
+++ src/main/java/ndt/java/spring/enties/Product.java
@@ -1,10 +1,14 @@
 package ndt.java.spring.enties;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +24,8 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int id;
 	
+	@Column(nullable = false, length = 128)
+	@NotNull @Length(min = 5, max = 128)
 	String name;
 	
 	float price;
