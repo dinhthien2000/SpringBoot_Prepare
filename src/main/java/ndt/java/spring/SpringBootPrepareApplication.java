424362385321f0127cf9e32ceca25276521260a3
Alan <thiencamau1804@gmail.com>
2023-10-22 02:03:15 +0700

    add config enabled web security in file main of spring boot application

--- src/main/java/ndt/java/spring/SpringBootPrepareApplication.java
+++ src/main/java/ndt/java/spring/SpringBootPrepareApplication.java
@@ -2,8 +2,9 @@ package ndt.java.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication @EnableWebSecurity(debug = true)
 public class SpringBootPrepareApplication {
 
 	public static void main(String[] args) {
