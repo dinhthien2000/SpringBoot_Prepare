2701143fa8a0828d71fb9852466fcefe7a27b4e7
Alan <thiencamau1804@gmail.com>
2023-10-22 03:09:27 +0700

    Update interface ProductRepository

--- src/main/java/ndt/java/spring/repository/ProductRepository.java
+++ src/main/java/ndt/java/spring/repository/ProductRepository.java
@@ -1,5 +1,11 @@
 package ndt.java.spring.repository;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ndt.java.spring.enties.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
