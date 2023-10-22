c66bfd78f64ab1848f235464d61f6a81e5cabdf2
Alan <thiencamau1804@gmail.com>
2023-10-22 21:43:25 +0700

    Update session management is stateless in Http Security filter chain

--- src/main/java/ndt/java/spring/config/WebSecurityConfig.java
+++ src/main/java/ndt/java/spring/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +14,11 @@ public class WebSecurityConfig {
 	static String[] LIST_MATCHER ={"/",""};
 	
 	@Bean
+	/*Because we don’t use classic web so disable CSRF and no session management needed*/
 	public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
 		return http.csrf(c->c.disable())
 			.authorizeHttpRequests(auth->auth.requestMatchers(LIST_MATCHER).permitAll())
+			.sessionManagement(t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.build();
 	}
 }
