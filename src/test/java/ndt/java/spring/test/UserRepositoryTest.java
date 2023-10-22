98d1b55bcf46fb576b329cae96cf457a45a7fcf5
Alan <thiencamau1804@gmail.com>
2023-10-23 01:49:49 +0700

    update BCryptPasswordEncoder

--- src/test/java/ndt/java/spring/test/UserRepositoryTest.java
+++ src/test/java/ndt/java/spring/test/UserRepositoryTest.java
@@ -7,13 +7,11 @@ import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 
 import lombok.RequiredArgsConstructor;
 import ndt.java.spring.enties.User;
 import ndt.java.spring.repository.UserRepository;
-
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -25,7 +23,7 @@ public class UserRepositoryTest {
 	
 	@Test
 	public void testCreateUser() {
-		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String password = encoder.encode("Abc@1234");
 		
 		User user = new User("ndtjava@gmail.com",password);
