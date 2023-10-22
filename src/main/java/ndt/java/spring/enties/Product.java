7510cfca330fd3f50bc591720017f0e487be5376
Alan <thiencamau1804@gmail.com>
2023-10-22 21:27:31 +0700

    Update @ToString to Entity Product

--- src/main/java/ndt/java/spring/enties/Product.java
+++ src/main/java/ndt/java/spring/enties/Product.java
@@ -13,11 +13,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "products")
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
+@ToString
 public class Product {
 	
 	@Id 
