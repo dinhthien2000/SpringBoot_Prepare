4aec5b5d22367aba64b49340ecea9986528d82bc
Alan <thiencamau1804@gmail.com>
2023-10-22 21:28:26 +0700

    Config authorize all request matcher is testing security

---
+++ src/main/java/ndt/java/spring/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package ndt.java.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+	
+	static String[] LIST_MATCHER ={"/",""};
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+		return http.csrf(c->c.disable())
+			.authorizeHttpRequests(auth->auth.requestMatchers(LIST_MATCHER).permitAll())
+			.build();
+	}
+}
