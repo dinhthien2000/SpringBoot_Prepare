0f6dfbf0c1954bb483c5c2c4d13d513a1205627e
Alan <thiencamau1804@gmail.com>
2023-10-30 01:44:57 +0700

    PasswordEncoder

--- src/main/java/ndt/java/spring/config/WebSecurityConfig.java
+++ src/main/java/ndt/java/spring/config/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,4 +23,11 @@ public class WebSecurityConfig {
 			.sessionManagement(t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.build();
 	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
 }
