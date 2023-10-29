fe1b2b731b1f97f1616962b0e98046b8b47f3fa1
Alan <thiencamau1804@gmail.com>
2023-10-30 02:42:43 +0700

    Here, the URI is /auth/login and we use an authentication manager to authenticate the user. That’s why we need to expose a bean of type AuthenticationManager in the security config class. In case the credential is invalid, a BradCredentialsException is thrown and the API returns HTTP status 401 (Unauthorized). If valid, it uses the JwtTokenUtil class to generate a new access token, which is then attached to the response object of type AuthResponse.

---
+++ src/main/java/ndt/java/spring/controller/AuthenAPI.java
@@ -0,0 +1,56 @@
+package ndt.java.spring.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import ndt.java.spring.config.WebSecurityConfig;
+import ndt.java.spring.dto.AuthenRequest;
+import ndt.java.spring.dto.AuthenResponse;
+import ndt.java.spring.enties.User;
+import ndt.java.spring.utils.JwtTokenUtil;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthenAPI {
+
+	final AuthenticationManager authenticationManager;
+	final JwtTokenUtil jwtUtil;
+	
+	/*
+	 * Ở đây, URI là /auth/login và chúng tôi sử dụng trình quản lý xác thực để xác thực người dùng. 
+	 * Đó là lý do tại sao chúng ta cần hiển thị một loại Bean AuthenticationManager trong lớp cấu hình bảo mật.
+		Trong trường hợp thông tin xác thực không hợp lệ, 
+		BradCredentialsException sẽ được ném ra và API trả về trạng thái HTTP 401 (Không được phép).
+		 Nếu hợp lệ, nó sử dụng lớp JwtTokenUtil để tạo mã thông báo truy cập mới,
+		  sau đó được gắn vào đối tượng phản hồi thuộc loại AuthResponse.
+	 * 
+	 * */
+	
+	@PostMapping("/auth/login")
+	public ResponseEntity<?> login(@RequestBody @Valid AuthenRequest request){
+		try {
+			Authentication authencation = authenticationManager.authenticate(
+						new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+					);
+			User user = (User) authencation.getPrincipal();
+			String accessToken = jwtUtil.generateAccessToken(user);
+			AuthenResponse response = new AuthenResponse(user.getEmail(), accessToken);
+			
+			return ResponseEntity.ok(response);
+			
+		} catch (BadCredentialsException e) {
+			// TODO: handle exception
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+	}
+}
