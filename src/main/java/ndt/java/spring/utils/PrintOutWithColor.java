ad45abd2e87ad9ec62cef511ab0abbd59d6ac59b
Alan <thiencamau1804@gmail.com>
2023-11-14 00:43:44 +0700

    Update method PrintOutWithColor.java

--- src/main/java/ndt/java/spring/utils/PrintOutWithColor.java
+++ src/main/java/ndt/java/spring/utils/PrintOutWithColor.java
@@ -2,22 +2,27 @@ package ndt.java.spring.utils;
 
 import java.security.Key;
 
+/*
+ * parameter 1: message
+ * parameter 2: color
+ * 
+ * */
 public class PrintOutWithColor {
 	
 	public static void print(String message, ColorSysoutUtil color) {
-		System.out.print(color + message + color.RESET);
+		System.out.print(color + message + ColorSysoutUtil.RESET);
 	}
 	
 	public static void println(String message, ColorSysoutUtil color) {
-		System.out.println(color + message + color.RESET);
+		System.out.println(color + message + ColorSysoutUtil.RESET);
 	}
 	
 	public static void println(Byte[] message, ColorSysoutUtil color) {
-		System.out.println(color + ""+ message + color.RESET);
+		System.out.println(color + ""+ message + ColorSysoutUtil.RESET);
 	}
 
 	public static void println(Key key, ColorSysoutUtil color) {
 		// TODO Auto-generated method stub
-		System.out.println(color + ""+ key + color.RESET);
+		System.out.println(color + ""+ key + ColorSysoutUtil.RESET);
 	}
 }
