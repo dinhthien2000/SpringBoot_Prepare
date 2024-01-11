f182bf44648781181557000c2cd1acf6363d8729
Alan <thiencamau1804@gmail.com>
2024-01-12 02:42:35 +0700

    update class User

--- src/main/java/ndt/java/spring/enties/User.java
+++ src/main/java/ndt/java/spring/enties/User.java
@@ -54,7 +54,7 @@ public class User implements UserDetails{
 	@ManyToMany
 	@JoinTable(
 			name = "user_roles",
-			joinColumns = @JoinColumn( name = "role_id"),
+			joinColumns = @JoinColumn( name = "user_id"),
 			inverseJoinColumns = @JoinColumn( name = "role_id")
 			)
 	private Set<Role> roles = new HashSet<>();
